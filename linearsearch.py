e74471c4cd22ef98fc90ffd491a9586eebc13f2c
Tara Alsyah <58825091+taraalsyah@users.noreply.github.com>
2025-02-20 18:44:41 +0700

    add2

--- linearsearch.py
+++ linearsearch.py
@@ -12,5 +12,5 @@ def LinearSearch(data,value):
             start += 1
 
 database = [23,21,2,1,5,9,34,56]
-print(f'Jumlah index1 data= {len(database) -1 }')
+print(f'Jumlah index12 data= {len(database) -1 }')
 LinearSearch(database,56)
